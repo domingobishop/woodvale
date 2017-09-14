636ad0b216964a574e10f936c7d168246b01ea44
Christopher Bishop <domingobishop@gmail.com>
2017-09-14 20:43:50 +0100

    Continue Shopping button on cart

--- functions-woo.php
+++ functions-woo.php
@@ -174,3 +174,12 @@ function woo_set_min_total() {
     }
 
 }
+
+add_action( 'woocommerce_before_cart_table', 'woo_add_continue_shopping_button_to_cart' );
+function woo_add_continue_shopping_button_to_cart() {
+    $shop_page_url = get_permalink( woocommerce_get_page_id( 'shop' ) );
+
+    echo '<div class="woocommerce-message">';
+    echo ' <a href="'.$shop_page_url.'" class="button">Continue Shopping →</a>';
+    echo '</div>';
+}
